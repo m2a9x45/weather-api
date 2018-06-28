e02f70b7cd776cb175a2339233cb2bd5d655bada
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2018-06-28 17:56:43 +0100

    update 3

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,30 +4,25 @@ var units = "metric";
 var url = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID=" + apikey + "&units=" + units;
 var weather = "";
 var temp = "";
+
 // url quare in full
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=2c060adb91b464e04545d6dafa96fbcc&units=metric
 
 function setup() {
-
   createCanvas(500,500);
-  // background(174, 174, 175);
-
   loadJSON(url,gotData);
-
 }
 
 function draw() {
-  textSize(64);
-  text(temp.toString(),250,250,70,80);
-
-
+  background(100,100,100);
 
   if (weather == "Clear") {
-    // background(255, 247, 43);
+    background(255, 247, 43);
   }
-  else {
-    // background(174, 174, 175);
-  }
+
+  textSize(64);
+  text(temp.toString() + '\xB0' + "C",0,0,500,500);
+  textAlign(CENTER,CENTER);
 }
 
 function gotData(data){
